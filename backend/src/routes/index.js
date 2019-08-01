b89edf43d4af643b3b3bedd61f291b5fed27f6de
nora <noraselenyi@gmail.com>
2019-08-01 12:32:27 +0200

    feat(backend): register done

---
+++ backend/src/routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const { userRegister } = require('../controllers/register-controller');
+
+
+const router = express.Router();
+
+router.post('/register', userRegister);
+
+
+
+module.exports = router;
