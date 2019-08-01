bf4c1a7c0202adfa748c9e2e4defe2cbd4f0904a
Monden8 <soma.feigl@gmail.com>
2019-08-01 11:42:56 +0200

    added pipestamp && angMaterial

--- OnlineWebShop/src/app/Pipes/timestamp.pipe.ts
+++ OnlineWebShop/src/app/Pipes/timestamp.pipe.ts
@@ -1,12 +1,38 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({
-  name: 'timestamp'
-})
-export class TimestampPipe implements PipeTransform {
+@Pipe({ name: 'elapsedTime' })
+export class PipeService implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  timeNow: number;
+  elapsedTime: number;
+  year: number = 31556926000;
+  month: number = 2629743000;
+  day: number = 86400000;
+  hour: number = 3600000;
+  yearsAgo: number;
+  monthsAgo: number;
+  daysAgo: number;
+  hoursAgo: number;
+
+  constructor() { }
+
+  transform(actionTimeStamp: number): string {
+    this.timeNow = Date.now();
+    this.elapsedTime = this.timeNow - actionTimeStamp;
+    this.yearsAgo = Math.floor(this.elapsedTime / this.year);
+    this.monthsAgo = Math.floor(this.elapsedTime / this.month);
+    this.daysAgo = Math.floor(this.elapsedTime / this.day);
+    this.hoursAgo = Math.floor(this.elapsedTime / this.hour);
+    if (this.elapsedTime > this.year) {
+      return (`${this.yearsAgo} years ago.`);
+    } else if (this.elapsedTime < this.year && this.elapsedTime > this.month) {
+      return (`${this.monthsAgo} months ago.`);
+    } else if (this.elapsedTime < this.month && this.elapsedTime > this.day) {
+      return (`${this.daysAgo} days ago.`);
+    } else if (this.elapsedTime < this.day && this.elapsedTime > this.hour) {
+      return (`${this.hoursAgo} hours ago.`);
+    } else if (this.elapsedTime < this.hour) {
+      return (`Just now.`);
+    }
   }
-
 }
--- OnlineWebShop/src/app/app.module.ts
+++ OnlineWebShop/src/app/app.module.ts
@@ -1,15 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Components/login/login.component';
 import { RegisterComponent } from './Components/register/register.component';
 import { FrontpageComponent } from './Components/frontpage/frontpage.component';
-import { TimestampPipe } from './Pipes/timestamp.pipe';
+import { PipeService } from './Pipes/timestamp.pipe';
 import { TrolleyComponent } from './Components/trolley/trolley.component';
 import { SearchBarComponent } from './Components/search-bar/search-bar.component';
 import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AuthenticationService } from "./Services/authentication.service";
+import { AuthInterceptor } from "src/app/authentication.interceptor";
+import { MatCardModule } from "@angular/material/card";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatListModule } from "@angular/material/list";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatSelectModule } from "@angular/material/select";
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatRippleModule,
+  MatCheckboxModule
+} from "@angular/material";
 
 @NgModule({
   declarations: [
@@ -17,16 +33,48 @@ import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
     LoginComponent,
     RegisterComponent,
     FrontpageComponent,
-    TimestampPipe,
     TrolleyComponent,
     SearchBarComponent,
     NavBarComponent
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatDividerModule,
+    MatListModule,
+    MatDialogModule,
+    HttpClientModule,
+    MatSelectModule
   ],
-  providers: [],
+
+  exports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    MatCheckboxModule,
+    MatSelectModule
+  ],
+  providers: [
+    AuthenticationService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+    PipeService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
