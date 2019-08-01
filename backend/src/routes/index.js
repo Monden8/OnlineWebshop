a7af6cebf3e171c931b2bad74ce5f18e32d35771
nora <noraselenyi@gmail.com>
2019-08-02 01:00:46 +0200

    fieat(backend): /buyintem done

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -6,6 +6,7 @@ const { userLogout } = require('../controllers/logout-controller');
 const { addItem } = require('../controllers/additem-controller');
 const { getItems } = require('../controllers/getitems-controller');
 const { getMySelling } = require('../controllers/getMyItems_forSale-controller');
+const { buyItem } = require('../controllers/buyitem-controller');
 
 const router = express.Router();
 
@@ -15,6 +16,6 @@ router.post('/logout', userLogout);
 router.post('/additem', addItem);
 router.get('/main', getItems);
 router.get('/myitems', getMySelling);
-
+router.get('/buyitem', buyItem)
 
 module.exports = router;
---
+++ backend/src/services/checkIfCostsMore-service.js
@@ -0,0 +1,23 @@
+const Items = require('../models/items.model');
+
+const checkIfCostsMore = (resultAndItem, newPrice) => new Promise((resolve, reject) => {
+  const currentPrice = resultAndItem.data[0].price;
+
+  if (currentPrice >= newPrice) {
+    reject(new Error('The current bid is higher.'))
+  } else {
+    Items.findOneAndUpdate({ _id: resultAndItem.data[0]._id }, { price: newPrice, highestBidder: resultAndItem.result.username }, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  }
+});
+
+
+module.exports = {
+  checkIfCostsMore,
+};
+
