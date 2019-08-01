3c649598705309cbdda1715f7043532b548edf23
nora <noraselenyi@gmail.com>
2019-08-01 19:55:37 +0200

    feat(backend): /additem done

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { userRegister } = require('../controllers/register-controller');
 const { userLogin } = require('../controllers/login-controller');
 const { userLogout } = require('../controllers/logout-controller');
+const { addItem } = require('../controllers/additem-controller');
 
 
 const router = express.Router();
@@ -10,7 +11,7 @@ const router = express.Router();
 router.post('/register', userRegister);
 router.post('/login', userLogin);
 router.post('/logout', userLogout);
-
+router.post('/additem', addItem);
 
 
 module.exports = router;
