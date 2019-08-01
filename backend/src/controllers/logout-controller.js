c3bb020a6b28f3aea9ca9b83b52112f4c6453936
Monden8 <soma.feigl@gmail.com>
2019-08-01 21:49:52 +0200

    feat(logout-cont.) .json

--- backend/src/controllers/logout-controller.js
+++ backend/src/controllers/logout-controller.js
@@ -6,7 +6,9 @@ const userLogout = (req, res) => {
 
   findUserByToken(rt)
     .then(result => eraseRefreshToken(result))
-    .then(() => { res.status(200).send('ok') })
+    .then(() => {
+      res.status(200).json({ message: 'ok' })
+    })
     .catch((err) => { res.status(400).json(err.message) });
 };
 
