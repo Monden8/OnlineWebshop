ff7cf7d4916c8063fcd8337e7aa696f5c62e409d
Monden8 <soma.feigl@gmail.com>
2019-08-01 10:18:17 +0200

    generated comp's&pipe&service

--- OnlineWebShop/src/app/app.module.ts
+++ OnlineWebShop/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { FrontpageComponent } from './Components/frontpage/frontpage.component';
+import { TimestampPipe } from './Pipes/timestamp.pipe';
+import { TrolleyComponent } from './trolley/trolley.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    FrontpageComponent,
+    TimestampPipe,
+    TrolleyComponent
   ],
   imports: [
     BrowserModule,
