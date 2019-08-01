93172a2fa040a525318cf89ddbf96d12e4ba2448
nora <noraselenyi@gmail.com>
2019-08-02 01:55:59 +0200

    feat(backend): /wishlist done

---
+++ backend/src/controllers/wishlist-controller.js
@@ -0,0 +1,19 @@
+
+const { findUserByToken } = require('../services/findUserByToken-service');
+const { findUsersHighestBids } = require('../services/findUsersHighestBids-service');
+
+
+const wishlist = (req, res) => {
+  const { rt } = req.body;
+
+  findUserByToken(rt)
+    .then(result => findUsersHighestBids(result))
+    .then(data => { res.status(200).json(data) })
+    .catch((err) => {
+      if (err) {
+        res.status(400).json({ status: 'error', message: err.message });
+      }
+    });
+};
+
+module.exports = { wishlist };
