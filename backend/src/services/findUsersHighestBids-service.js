ae3d61c070dc6ee802402cb4e6372571bb8f4d53
Monden8 <soma.feigl@gmail.com>
2019-08-02 07:58:05 +0200

    IDK

--- backend/src/services/findUsersHighestBids-service.js
+++ backend/src/services/findUsersHighestBids-service.js
@@ -2,7 +2,6 @@ const Items = require("../models/items.model");
 
 const findUsersHighestBids = result =>
   new Promise((resolve, reject) => {
-    console.log(result.username);
     Items.find({ highestBidder: result.username }, (err, data) => {
       if (err) {
         reject(err);
